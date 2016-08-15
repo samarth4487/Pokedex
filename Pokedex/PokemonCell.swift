dbdca858cfbe9feb3cb2ba5f95964dfd09ad6308
Samarth Paboowal <samarthpaboowal@Samarths-MacBook-Air.local>
2016-08-16 02:24:09 +0530

    Added all 718 pokemons in a collection view

---
+++ Pokedex/PokemonCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokemonCell.swift
+//  Pokedex
+//
+//  Created by Samarth Paboowal on 16/08/16.
+//  Copyright © 2016 Junkie Labs. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        self.nameLabel.text = pokemon.name.capitalizedString
+        self.thumbImg.image = UIImage(named: "\(pokemon.pokedexID)")
+        
+    }
+}
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -8,12 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var pokemon = [Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        parseCSV()
     }
-
+    
+    func parseCSV() {
+        
+        let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")
+        do {
+            let csv = try CSV(contentsOfURL: path!)
+            let rows = csv.rows
+            
+            for row in rows {
+                let pokeID = Int(row["id"]!)!
+                let name = row["identifier"]!
+                let poke = Pokemon(name: name, pokedexID: pokeID)
+                pokemon.append(poke)
+            }
+            
+        } catch let err as NSError {
+            print(err.description)
+        }
+        
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pokemon.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokemonCell", forIndexPath: indexPath) as? PokemonCell {
+            
+            let poke = pokemon[indexPath.row]
+            cell.configureCell(poke)
+            return cell
+        }
+        else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 
 }
 
