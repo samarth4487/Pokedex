02e5d64eacfb60d42efc2f18c5c525f853fdc1e9
Samarth Paboowal <samarthpaboowal@Samarths-MacBook-Air.local>
2016-08-17 01:50:11 +0530

    Modified the pokemon model and added connections for the pokemon detail view controller

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -12,9 +12,26 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var evolutionLabel: UILabel!
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var secondImage: UIImageView!
+    @IBOutlet weak var segmentController: UISegmentedControl!
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        nameLabel.text = pokemon.name.capitalizedString
     }
 
+    @IBAction func backButtonPressed(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
